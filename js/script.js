a121cd75dffb96a7eb2d45a6fcbc870fc9c5e913
nvg58 <nvangiap5894@gmail.com>
2014-07-15 23:01:40 +0700

    remove redundant js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-console.log($('.nav .dropdown'));
-$('.nav .dropdown').hover(function() {
-	$(this).addClass('open');	
-  	$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-  	console.log("zzz");
-	}, function() {
-  		$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();  	
-  	}  	
-);

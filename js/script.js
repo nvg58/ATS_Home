9b05f5f6265677d55a5bc8135c0659b4dc0411a5
nvg58 <nvangiap5894@gmail.com>
2014-07-17 16:37:33 +0700

    fix respondsive

--- js/script.js
+++ js/script.js
@@ -12,4 +12,10 @@ $( window ).load(function() {
 	}, function() {
 		/* Stuff to do when the mouse leaves the element */
 	});
+
+	var $stay_update = $('.stay-update');
+	console.log($('#newsletter .stay-update:before'));
+	console.log($stay_update.height());
+	$('head').append('<style>#newsletter .stay-update:before{border-width:' + $stay_update.height()/2 + 'px !important;}</style>');
+	// $('#newsletter .stay-update').before('content');.css('border-width', $stay_update.height()/2);
 });
